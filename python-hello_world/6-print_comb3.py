b545c3fb80ff7e9f6297741dccfe0ca66d19a357
root <jaydenrishi@gmail.com>
2023-07-18 04:33:34 -0700

    6. Inventing is a combination of brains and materials. The more brains you use, the less material you need[Fixed]

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,6 @@
-#! /usr/bin/python3
-for i in range(0, 99):
-    if i < 10:
-        print('0{}'.format(i), end=", ")
+#!/usr/bin/python3
+for i in range(90):
+    if (i % 10 <= i // 10):
+        continue
     else:
-        print(i ,end=", ")
+        print("{:02d}".format(i), end="\n" if i == 89 else ", ")
